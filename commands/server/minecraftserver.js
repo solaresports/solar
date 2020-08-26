befc0f8453b8fa2a898dba6dcc84245a58848ef4
solaresports <70237441+solaresports@users.noreply.github.com>
2020-08-26 17:51:12 +0200

    init

--- commands/server/minecraftserver.js
+++ commands/server/minecraftserver.js
@@ -1,15 +1,41 @@
 ﻿const discord = require("discord.js");
 module.exports = {
-    name: 'mcserver',
+    name: 'arkserver',
     category: 'server',
-    description: 'mcserver',
+    description: 'arkserver',
     run: async (bot, message, args, guild) => {
 
         var botIcon = bot.user.displayAvatarURL;
 
         var botEmbed = new discord.MessageEmbed()
             .setTitle("Minecraft - Survival")
-            .setDescription(`Coming soon`)
+            .setDescription(`    
+            **Information**
+            We have our own minecraft server.
+            This is a small and friendly survival community.
+            PVP is disabled.
+            
+            **Server Settings**
+            > - Difficulty: Normal
+            > - Version: 1.16.2
+            > - PVP: Disabled
+            > - Griefprotection: Enabled
+            > - Chestlock: Enabled
+            
+            **Server World**
+            > - Size: Normal biomes
+            > - Map Version: 1.16.2
+            > 
+            **Server Plugins**
+            > - Essentials
+            > - Luckyperms
+            > - LWC Chestlock
+            > - Coreprotection
+            > - Solar Moderation
+            > - Solar Events
+            
+            **Server IP**
+            > **Survival IP:** solar.mclogin.nl`)
             .setColor("#ba90f1")
 
         message.delete().catch(err => console.log(err));
