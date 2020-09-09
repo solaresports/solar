984b9a345bd992e27cdcd3e9bab218a89f348fea
solaresports <70237441+solaresports@users.noreply.github.com>
2020-09-09 18:41:26 +0200

    Create polls.js

---
+++ commands/announcements/polls.js
@@ -0,0 +1,35 @@
+const Discord = require("discord.js");
+module.exports = {
+    name: 'poll',
+    category: 'announcements',
+    description: 'poll',
+    run: async (bot, message, args) => {
+
+        const discord = require("discord.js");
+
+            const sayMessage = args.join(" ");
+            let dUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+            if (!dUser) return message.channel.send("Can't find user!")
+            if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You can't you that command!")
+            let dMessage = args.join(" ").slice(22);
+            let successfullyembed = new Discord.MessageEmbed()
+            message.delete().catch(err => console.log(err));
+
+            var botIcon = bot.user.displayAvatarURL;
+
+            var botEmbed = new discord.MessageEmbed()
+                .setTitle("SOLAR - POLLS")
+                .setDescription(`**Posted By:** ${message.author}
+                                 ${dMessage}
+
+                                 `)
+                .setColor("#71deeb")
+                .setFooter("React on this message to vote")
+                .setThumbnail("https://icons.iconarchive.com/icons/custom-icon-design/pretty-office-6/48/polls-icon.png")
+
+            dUser.send(`${dUser} A poll has been made by ${message.author}
+                       poll made by ${ message.author}: ${dMessage}`)
+
+            return message.channel.send(botEmbed);
+        }
+}
