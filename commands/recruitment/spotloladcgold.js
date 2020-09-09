75e71529fb34f984a59d66ba8a4bc7892499b02e
solaresports <70237441+solaresports@users.noreply.github.com>
2020-09-09 05:26:52 +0200

    Update spotloladcgold.js

--- commands/recruitment/spotloladcgold.js
+++ commands/recruitment/spotloladcgold.js
@@ -11,10 +11,11 @@ module.exports = {
             .setTitle("League of Legends - Recruitment")
             .setDescription(`There is a position available in an existing team.
                              This team is looking for a Toplaner.
-                             Direct message the person above this message to plan a tryout.`)
+                             Direct message @Aspect#9273 to plan a tryout.`)
             .setColor("#4a77e8")
             .setThumbnail(botIcon)
-            .setImage("https://img.rankedboost.com/wp-content/uploads/2014/09/Season_2019_-_Gold_1.png")
+            .setImage("https://cdn.discordapp.com/attachments/753079584475578408/753093950113579119/Season_2019_-_Gold_1.png")
+            .setTimestamp
 
         message.delete().catch(err => console.log(err));
         return message.channel.send(botEmbed);
