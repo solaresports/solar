f797d96fb8c84e169243aede4b8dbe7f8eb8af30
solaresports <70237441+solaresports@users.noreply.github.com>
2020-09-09 05:51:31 +0200

    Update spotloladcgold.js

--- commands/recruitment/spotloladcgold.js
+++ commands/recruitment/spotloladcgold.js
@@ -1,24 +1,34 @@
-﻿const discord = require("discord.js");
+﻿const Discord = require("discord.js");
 module.exports = {
-    name: 'loladcgold',
+    name: 'lolgoldadc',
     category: 'recruitment',
-    description: 'loladcgold',
-    run: async (bot, message, args, guild) => {
+    description: 'lolgoldadc',
+    run: async (bot, message, args) => {
+
+        const discord = require("discord.js");
+
+        message.delete().catch(err => console.log(err));
+        const sayMessage = args.join(" ");
+        let dUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+        if (!dUser) return message.channel.send("Can't find user!")
+        if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You can't you that command!")
+        let dMessage = args.join(" ").slice(22);
+        let successfullyembed = new Discord.MessageEmbed()
+        message.channel.send(sayMessage);
 
         var botIcon = bot.user.displayAvatarURL;
 
         var botEmbed = new discord.MessageEmbed()
             .setTitle("Direct Recruitment")
-            .setDescription(`There is a position available in an existing team.
-                               
-                             **Game:** League of Legends
-                             **Role:** Toplane
-                             **Elo:** Gold`)
+            .setDescription(`There is a position available in an existing team.`)
             .setColor("#4a77e8")
             .setThumbnail(botIcon)
-            .setFooter("You can always apply in (📌join-a-team) This are just direct open spots.")
+            .setImage("https://cdn.discordapp.com/attachments/753079584475578408/753093950113579119/Season_2019_-_Gold_1.png")
+            .setImage("https://cdn.discordapp.com/attachments/753079584475578408/753099897984778321/ap550x55012x121transparentt.u6.png")
 
-        message.delete().catch(err => console.log(err));
+        dUser.send(`Hey ${dUser} you placed used a command ${dMessage}
+        > Recruitment Messaged Added`)
+
         return message.channel.send(botEmbed);
     }
 }
