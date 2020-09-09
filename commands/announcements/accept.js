51de63b488dc89c04f76bc6f6818026891b21628
solaresports <70237441+solaresports@users.noreply.github.com>
2020-09-09 16:51:05 +0200

    Update accept.js

--- commands/announcements/accept.js
+++ commands/announcements/accept.js
@@ -27,15 +27,15 @@ module.exports = {
                 .setColor("#6cf567")
                 .setThumbnail("https://icons.iconarchive.com/icons/hopstarter/sleek-xp-basic/48/Ok-icon.png")
 
-            dUser.send(`Hey ${dUser} a application status update ${dMessage}
-        > Congratulations!
-        
-        > You are hereby officially accepted as a server member!
-        > We will start looking for a team for you in short order, but for now you have access to all our channels and voice channels.
-        > Once we have found a team for you, we will let you know.
+            dUser.send(`${dUser} a application status update by ${message.author}
+        You are hereby officially accepted as a server member!
+        We will start looking for a team for you in short order, but for now you have access to all our channels and voice channels.
+        Once we have found a team for you, we will let you know.
 
-        > Do you have questions?
-        > The easiest way to do this is by creating a ticket or by joining the voice support waiting room.`)
+        Do you have questions?
+        The easiest way to do this is by creating a ticket or by joining the voice support waiting room.
+
+        Notes made by ${message.author}: ${dMessage}`)
 
             message.author.send(`${message.author} You have accepted ${dUser} as a member`)
 
