1ed5a10142b31a3330e0a8155c6687f4406ecfdd
solaresports <70237441+solaresports@users.noreply.github.com>
2020-09-09 05:45:17 +0200

    Update spotloladcgold.js

--- commands/recruitment/spotloladcgold.js
+++ commands/recruitment/spotloladcgold.js
@@ -10,13 +10,14 @@ module.exports = {
         var botEmbed = new discord.MessageEmbed()
             .setTitle("Direct Recruitment")
             .setDescription(`There is a position available in an existing team.
+                               
                              **Game:** League of Legends
                              **Role:** Toplane
                              **Elo:** Gold`)
             .setColor("#4a77e8")
             .setThumbnail(botIcon)
             .setImage("https://cdn.discordapp.com/attachments/753079584475578408/753093950113579119/Season_2019_-_Gold_1.png")
-            .setFooter("You can always apply in 📌join-a-team to join a team. This are just direct open spots.")
+            .setFooter("You can always apply in (📌join-a-team) This are just direct open spots.")
 
         message.delete().catch(err => console.log(err));
         return message.channel.send(botEmbed);
