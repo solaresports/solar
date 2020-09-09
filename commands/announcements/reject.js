6a9d5c5639fccf8adccd95bc521e021e2a1f4c25
solaresports <70237441+solaresports@users.noreply.github.com>
2020-09-09 07:06:17 +0200

    Update reject.js

--- commands/announcements/reject.js
+++ commands/announcements/reject.js
@@ -7,13 +7,13 @@ module.exports = {
 
         const discord = require("discord.js");
 
+            const sayMessage = args.join(" ");
             let dUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
             if (!dUser) return message.channel.send("Can't find user!")
             if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You can't you that command!")
             let dMessage = args.join(" ").slice(22);
             let successfullyembed = new Discord.MessageEmbed()
             message.delete().catch(err => console.log(err));
-            message.channel.send(sayMessage);
 
             var botIcon = bot.user.displayAvatarURL;
 
