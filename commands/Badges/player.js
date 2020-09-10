e4e3f48c2099c28c9bd0b0bdf855eeb2a16adc86
solaresports <70237441+solaresports@users.noreply.github.com>
2020-09-10 14:49:58 +0200

    Update player.js

--- commands/Badges/player.js
+++ commands/Badges/player.js
@@ -29,6 +29,7 @@ module.exports = {
                                  `)
             .setColor("#99e8d2")
             .setThumbnail("https://cdn.discordapp.com/attachments/753079584475578408/753593830573473892/SolarPlayer.png")
+            .setImage("https://cdn.discordapp.com/attachments/753079584475578408/753593830573473892/SolarPlayer.png")
 
         dUser.send(`${dUser} Badge added to the discord ${message.author}
                        Message made by ${ message.author}: ${dMessage}`)
