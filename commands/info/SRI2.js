c110eff4d186fd19ceb65b2803e12e4326080e54
solaresports <70237441+solaresports@users.noreply.github.com>
2020-12-23 05:40:43 +0100

    Update SRI2.js

--- commands/info/SRI2.js
+++ commands/info/SRI2.js
@@ -8,9 +8,9 @@ module.exports = {
         var botIcon = bot.user.displayAvatarURL;
 
         var botEmbed = new discord.MessageEmbed()
-            .setTitle("Need our help?")
+            .setTitle("Vraag om hulp")
             .setDescription(`    
-            > Do you need help from our staff members? Or do you want to create a report? Then go to the channel: ask for help. and create a ticket. Once you have done this, we will try to help you as soon as possible!`)
+            Als je hulp nodig hebt, mag je een @moderator taggen in de chat. Deze zullen reageren op jou vraag en dit zo snel mogelijk behandelen. Keep in mind, dat iedereen dit vrijwillig doet. En dus ook niet altijd direct kan antwoorden. Graag vragen wij ook respect voor de moderators.!`)
             .setColor("#8a3df0")
 
         message.delete().catch(err => console.log(err));
