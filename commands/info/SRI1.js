ebe488d62940084343463c8f48ede3b3c9599d4c
solaresports <70237441+solaresports@users.noreply.github.com>
2020-12-23 05:33:22 +0100

    info update
    update

--- commands/info/SRI1.js
+++ commands/info/SRI1.js
@@ -8,15 +8,15 @@ module.exports = {
         var botIcon = bot.user.displayAvatarURL;
 
         var botEmbed = new discord.MessageEmbed()
-            .setTitle("Server Rules")
+            .setTitle("Discord Regels")
             .setDescription(`    
-            > **1:** All text channels are in English. Speak only this language in our chats.
-            > **2:** No harassment of other players, this includes: Racism, Sexism, Xenophobia, Transphobia, Homophobia, ect.
-            > **3:** No ads from other discord and from websites in our chats.
-            > **4:** Keep our voice channels friendly. Don't be toxic and behave yourself.
-            > **5:** Do not spam in our chats. This is tedious and difficult.
-            > **6:** No NSFW content of conversations about illegal activities.
-            > **7:** Staff has the final say. Listen and respect the employees who keep the server running.`)
+            > **1:** Gedraag je in de chat, ben aardig tegen elkaar en laat iedereen in zijn of haar waarden.
+            > **2:** Geen discriminatie, Racisme, of andere onnodige uitingen.
+            > **3:** Staff is hier om alles rustig te houden, luister dan ook naar hun.
+            > **4:** Maak geen reclame in deze discord. Dit is niet toegestaan.
+            > **5:** Praten over ingame dingen van TDA is niet toegestaan. houd dit stream related, Geen meta.
+            > **6:** Vraag niet om een rang, en of andere goederen.
+            > **7:** Verkoop geen goederen in onze discord.`)
             .setColor("#8a3df0")
 
         message.delete().catch(err => console.log(err));
