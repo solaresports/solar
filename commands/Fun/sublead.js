29196b64292ad1482b2dd6a732e9ab438d470a47
solaresports <70237441+solaresports@users.noreply.github.com>
2021-01-01 23:37:38 +0100

    Update sublead.js

--- commands/Fun/sublead.js
+++ commands/Fun/sublead.js
@@ -8,14 +8,14 @@ module.exports = {
         var botIcon = bot.user.displayAvatarURL;
 
         var botEmbed = new discord.MessageEmbed()
-            .setTitle("Twitch Sub Lead")
+            .setTitle("Greatest Wizard")
             .setDescription(`> Current Lead:
-            > Namlessarcher
-            > Follw him: https://www.twitch.tv/namlessarcher
-            > Lead Amount: 1
+            > Geen
+            > Follw him: ...
+            > Lead Amount: ...
 
-            > Wanna be the new sub lead?
-            > Dont wait and go to: https://www.twitch.tv/se_aspect`)
+            > Wil je de nieuwe sub lead zijn?
+            > Dan kan je hier abboneren: https://www.twitch.tv/1avoc1`)
             .setColor("#6441a5")
             .setImage("https://cdn.discordapp.com/attachments/743163303110639637/747288553519841300/1024px-Twitch_logo_2019.svg.png")
 
